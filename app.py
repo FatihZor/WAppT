1c66088fbd2da42659f648c2ecc1256bdc38b291
Fatih Zor <fatih.zor@ogrenci.karatay.edu.tr>
2020-04-26 20:08:52 +0300

    email validation

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import logging
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import current_user, login_required, LoginManager, login_user, logout_user
 from mongoengine.errors import NotUniqueError
+import re
+
+regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+def check_email(email):  
+    if(re.search(regex,email)):  
+        return True 
+    else:  
+        return False 
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
@@ -13,6 +21,8 @@ app = Flask(__name__)
 login_manager.init_app(app)
 app.secret_key = 'secretkey'
 login_manager.login_view = 'login'
+
+
 
 @app.route('/<path:path>')
 def send_assets(path):
@@ -33,16 +43,21 @@ def login():
     elif request.method == 'POST':
         p_email = request.form['email']
         p_password = request.form['password']
-        try:
-            user = Users.objects(email = p_email).first()
-            if user is None or not check_password_hash(user.password, p_password):
-                flash("Username or password wrong")
+        if check_email(p_email):
+            try:
+                user = Users.objects(email = p_email).first()
+                if user is None or not check_password_hash(user.password, p_password):
+                    flash("Username or password wrong")
+                    return redirect(url_for('login'))
+                else:
+                    login_user(user)
+                    return redirect(url_for('index'))
+            except:
+                flash("Something went wrong")
                 return redirect(url_for('login'))
-            else:
-                login_user(user)
-                return redirect(url_for('index'))
-        except:
-            flash("Something went wrong")
+        else:
+            flash("Please enter a valid email address")
+            return redirect(url_for('login'))
     return True
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -53,17 +68,21 @@ def register():
         p_name = request.form['name']
         p_password = request.form['password']
         p_email = request.form['email']
-        try:
-            user = Users()
-            user.name = p_name
-            user.email = p_email
-            user.user_type = "Free"
-            user.password = generate_password_hash(p_password)
-            user.save()
-            login_user(user)
-        except NotUniqueError as e:
-            flash("E-Mail or Username is taken")
-            return redirect(url_for("register"))
+        if check_email(p_email):
+            try:
+                user = Users()
+                user.name = p_name
+                user.email = p_email
+                user.user_type = "Free"
+                user.password = generate_password_hash(p_password)
+                user.save()
+                login_user(user)
+            except NotUniqueError as e:
+                flash("E-Mail is taken")
+                return redirect(url_for("register"))
+        else:
+            flash("Please enter a valid email address")
+            return redirect(url_for('register'))
         return redirect(url_for('index'))
     return True
 
